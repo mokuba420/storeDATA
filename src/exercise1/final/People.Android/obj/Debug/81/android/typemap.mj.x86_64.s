d371721b279f3852e9fa40d84957843f6892d6f0
mokuba420 <56065996+mokuba420@users.noreply.github.com>
2019-11-22 11:06:29 +0200

    updata

---
+++ src/exercise1/final/People.Android/obj/Debug/81/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: e5e04a845b3fa298ab3d17a3cf060ebc213b7856 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1320
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 345841
+	.include	"typemap.mj.inc"
---
+++ src/exercise1/final/People/Models/People .cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace People.Models
+{
+    public class People
+    {
+        [Table("people")]
+        public class Person
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+
+            [MaxLength(250), Unique]
+            public string Name { get; set; }
+
+
+        }
+    }
+}
